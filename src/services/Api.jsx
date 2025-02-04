7837b597b4bd65d1047d1b6b57a2976c22299d44
Ezequias Soares <87997012+EzequiasSoares1@users.noreply.github.com>
2025-02-04 11:29:07 -0300

    Update Api.jsx

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://ad-monteiro-back.onrender.com",
+    
+     baseURL: "https://ad-monteiro-back.onrender.com",
 });
-
 
 export const login = async (data) => {
     return await api.post("/auth", data);
@@ -24,9 +24,9 @@ export const getAllEvent = async (token) => {
 
 };
 
-export const getReport = async (nameEvent, token) => {
+export const getReport = async (id, token) => {
 
-    return await api.get(`/reportsRootRestricts/${nameEvent}`, {
+    return await api.get(`/reportsRootRestricts/${id}`, {
         headers: {
             Authorization: `Bearer ${token}`
         }
@@ -38,10 +38,16 @@ export const postCreatePayment = async (paymentData) => {
     return await api.post("/createPay", paymentData);
 };
 
+// Aqui criamos a preferência de pagamento para o Mercado Pago
+export const postCreatePreference = async (evento) => {
+    return await api.post("/createPay/create_preference", evento);
+};
+
 export default {
     login,
     getEventActive,
     getAllEvent,
     postCreatePayment,
     getReport,
+    postCreatePreference,
 };
